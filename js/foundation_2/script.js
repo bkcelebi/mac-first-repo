f5383ea2304a7a9dbc94c94579a681e15db3545a
KC_Dev <burakkcelebi@hotmail.com>
2022-12-07 20:27:16 +0000

    I have gone through logical operators

--- js/foundation_2/script.js
+++ js/foundation_2/script.js
@@ -55,3 +55,24 @@ document.getElementById("sliceNegative").innerHTML = twoLine.slice(-13, -8);
 
 document.getElementById("substr").innerHTML = twoLine.substr(2, 4);
 
+
+
+// check the login
+
+
+let user = prompt("Who are you?");
+
+if (user === "Admin"){
+    let userPw = prompt("What is the password");
+    if (userPw === "TheMaster"){
+        alert("Welcome!");
+    }else if(userPw === null || userPw === ""){
+        alert("Canceled");
+    }else{ 
+        alert("Wrong Password");
+    };
+} else if(user === null || user === ""){
+    alert("Canceled");
+} else{
+    alert("I don't know you");
+};
