67207bdef5a0fa5f5ecc54aff4144e04c86c81cd
KC_Dev <burakkcelebi@hotmail.com>
2022-12-21 20:40:50 +0000

    practiced. Basic functions are practiced.
    Half of the foundation 3 is now done. Basic function characteristics are practiced.

--- js/foundation_3/index.js
+++ js/foundation_3/index.js
@@ -23,3 +23,32 @@ function myFunction(){
 const myButton = document.getElementById("fnc");
 myButton.addEventListener("click", myFunction);
 
+//anonymous function and arrow function
+
+//anonymous function
+// function logKey(event){
+//     // console.log(`You pressed ${event.key}.`);
+//     document.getElementById("txtOutput").innerText += `You pressed ${event.key}.`;
+// };
+
+// const txt = document.getElementById("txt");
+// txt.addEventListener("keydown", logKey);
+
+//Arrow function
+
+const txt = document.getElementById("txt");
+txt.addEventListener("keydown", (event) => {
+    document.getElementById("txtOutput").innerText += `You pressed ${event.key}.`;
+});
+
+
+const originals = [1, 2, 3];
+const doubled = originals.map((item) => item * 2);
+document.getElementById("mapping").innerText = doubled;
+
+//basic return
+function basicReturn(animal){
+    return `${animal} is my favorite animal`
+};
+
+document.getElementById("rtrn").innerText = basicReturn("roly");
