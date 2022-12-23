2258d2a3bc4db897fa375d344c6d8b752ca567ac
KC_Dev <burakkcelebi@hotmail.com>
2022-12-23 22:17:10 +0000

    learnt! Arrow functions and call stack are done
    The rest of the Fundamentals Part 3 is done. Arrow functions, call stack and stack overflow are learnt.

--- js/foundation_3/index.js
+++ js/foundation_3/index.js
@@ -46,9 +46,94 @@ const originals = [1, 2, 3];
 const doubled = originals.map((item) => item * 2);
 document.getElementById("mapping").innerText = doubled;
 
+let func = (name) => {
+    // let name = "Burky";
+
+    document.getElementById("arrowFunc").innerText = name;
+};
+
+let name = prompt("What is your name", "Anonymous");
+func(name);
+
+let sum = (a, b) => a + b;
+document.getElementById("sum").innerText = sum(2, 4);
+
+let sqr = a => a * a;
+let sq = a => {
+    return a*a;
+};
+document.getElementById("sqr").innerText = sq(4);
+
+
+let age = prompt("How old are you?", " ");
+
+let permissionCheck = (age > 18) ?
+    () => document.getElementById("age").innerText = "Permission is granted" :
+    () => document.getElementById("age").innerText = "Permission is declined";
+
+permissionCheck(age)
+
+
+//before 
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes();
+//     else no();
+// }
+  
+// ask(
+//     "Do you agree?",
+//     function() { alert("You agreed."); },
+//     function() { alert("You canceled the execution."); }
+// );
+
+//after
+
+let ask = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+};
+
+ask(
+    "Do you agree?",
+    () => { alert("You agreed."); },
+    () => { alert("You canceled the execution."); }
+);
+
+
 //basic return
 function basicReturn(animal){
     return `${animal} is my favorite animal`
 };
 
 document.getElementById("rtrn").innerText = basicReturn("roly");
+
+
+//Call stack
+
+let sm = (a, b) => a + b;
+let av = (a, b) => sm(a, b)/2;
+document.getElementById("av").innerText = av(12, 2);
+
+//stack overflow
+
+// let stackOverflow = () => stackOverflow();
+// stackOverflow();
+
+
+//Assignment
+
+//1
+let add7 = a => a + 7;
+document.getElementById("as1").innerText = add7(5);
+
+//2
+let multiply = (a, b) => a * b;
+document.getElementById("as2").innerText = multiply(2, 6);
+
+//3
+let capitalise = (aString) => aString.toUpperCase();
+document.getElementById("as3").innerText = capitalise("burak");
+
+//4
+let lastLetter = (lstLetter) => lstLetter.slice(-1);
+document.getElementById("as4").innerText = lastLetter("Rachy");
