84c7aa954a0ca8316aa0e71800a331910e073e11
KC_Dev <burakkcelebi@hotmail.com>
2022-12-29 22:31:28 +0000

    Learnt and Created! Problem solving is learnt and FizzBuzz game is created
    How to break the complex problems down into smaller ones is learnt and practiced on FizzBuzz game

---
+++ js/problem_solving/index.js
@@ -0,0 +1,37 @@
+"use strict"
+
+let startNumber = 1;
+
+let fizzBuzz = startNumber => {
+    if (startNumber % 3 === 0){
+        if (startNumber % 5 === 0){
+            startNumber = "FizzBuzz";
+        }else{
+            startNumber = "Fizz";
+        }
+    }else if(startNumber % 5 === 0){
+        startNumber = "Buzz";
+    }else{
+        startNumber = startNumber;
+    }
+    return startNumber;
+};
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    let userNumber = parseInt(document.getElementById("number").value);
+
+    if (userNumber <= 0){
+        document.getElementById("fizzBuzz").innerText = "Please enter a number greater than 0";
+    }else{
+        while (startNumber <= userNumber){
+            document.getElementById("fizzBuzz").innerText += " " + fizzBuzz(startNumber);
+            startNumber++
+        }  
+    };
+});
+
+
+
+
