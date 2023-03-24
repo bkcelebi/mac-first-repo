319867103c8b354ddc0bf0ab262fb166cdb7524f
KC_Dev <burakkcelebi@hotmail.com>
2023-03-24 23:25:33 +0000

    Learnt! Basic native form controls are practiced!
    Basic native form controls on MDN Web Docs (https://developer.mozilla.org/en-US/docs/Learn/Forms/Basic_native_form_controls) are practiced

---
+++ intermediate/HTML/fromBasics/formActiveLearnings/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const isDisabled = document.querySelector("#disable");
+const paymentSubmissionButton = document.querySelector(
+  "#paymentSubmissionButton"
+);
+const timestamp = document.querySelector("#timestamp");
+
+// if (isDisabled.checked == true) console.log(1);
+// else console.log(0);
+
+isDisabled.addEventListener("click", () => {
+  if (isDisabled.checked == false) {
+    paymentSubmissionButton.setAttribute("disabled", true);
+  } else {
+    paymentSubmissionButton.removeAttribute("disabled");
+  }
+
+  console.log(isDisabled.value);
+});
+
+paymentSubmissionButton.addEventListener("click", () => {
+  timestamp.value =
+    `${new Date().getDate()}/` +
+    `${new Date().getMonth() + 1}/` +
+    `${new Date().getFullYear()} ` +
+    `${new Date().getHours()}:` +
+    `${new Date().getMinutes()}:` +
+    `${new Date().getSeconds()}`;
+  console.log(timestamp.value);
+});
