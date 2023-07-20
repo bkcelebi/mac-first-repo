ff5b19fd6ffbabb7b8b010e4f73dd76fe02e1142
KC_Dev <burakkcelebi@hotmail.com>
2023-07-20 20:48:30 +0100

    Factory Functions was started

---
+++ intermediate/js/factoryFunctions/index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// console.log("Hello");
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// Person.prototype.greeting = function () {
+//   console.log(`Hi ${this.name}`);
+// };
+
+// const newPerson = new Person("Burak", 28);
+
+// newPerson.greeting();
+
+// const person = (name, age) => {
+//   const greeting = () => console.log(`Hi ${name}`);
+//   return { name, age, greeting };
+// };
+
+// const burak = person("Burak", 28);
+// const rachael = person("Rachael", 28);
+
+// console.log(burak.name);
+// burak.greeting();
+// rachael.greeting();
+
+// let a = 17;
+
+// const func = (x) => {
+//   let a = x;
+//   return a;
+// };
+
+// func(99);
+
+// console.log(a);
+// console.log(func(99));
+
+// const name = "Maynard";
+// const color = "red";
+// const number = 34;
+// const food = "rice";
+
+// console.log({ name, color, number, food });
+// console.log(name, color, number, food);
