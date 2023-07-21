e58f7eb236b9116efdbb6d0c5aacaf4616d545df
KC_Dev <burakkcelebi@hotmail.com>
2023-07-21 20:45:31 +0100

    refresher on scoping and public and private functions are learnt

--- intermediate/js/factoryFunctions/index.js
+++ intermediate/js/factoryFunctions/index.js
@@ -34,7 +34,7 @@
 //   return a;
 // };
 
-// func(99);
+// // func(99);
 
 // console.log(a);
 // console.log(func(99));
@@ -46,3 +46,91 @@
 
 // console.log({ name, color, number, food });
 // console.log(name, color, number, food);
+
+// const sayHello = (name) => {
+//   const text = `Hello, ${name}`;
+//   return () => {
+//     console.log(text);
+//   };
+// };
+
+// const hiBurak = sayHello("Burak");
+// hiBurak();
+// or sayHello("Burak")();
+
+// const sayHello = (name) => {
+//   const text = `Hello, ${name}`;
+//   return text;
+// };
+
+// console.log(sayHello("Burak"));
+
+// const links = document.querySelectorAll("li");
+
+// for (let i = -1; i < links.length; i++) {
+//   console.log(this);
+// }
+
+// for (let i = 0; i < links.length; i++) {
+//   (function () {
+//     console.log(this);
+//   }).call(links[i]);
+// }
+
+// var width = 100;
+// console.log(width); // 100
+// width = 200;
+// console.log(width); // 200
+
+// var width = 100;
+
+// function setWidth() {
+//   var width = 100;
+//   console.log(width);
+// }
+// setWidth();
+// console.log(width); // error, width is not defined
+
+// var age = 100;
+// if (age > 12) {
+//   var dogYears = age * 7;
+//   console.log(`You are ${dogYears} dog years old!`);
+// }
+
+// console.log(dogYears);
+
+// var age = 100;
+// if (age > 12) {
+//   const dogYears = age * 7;
+//   console.log(`You are ${dogYears} dog years old!`);
+// }
+
+// console.log(dogYears);
+
+const FactoryFunction = (string) => {
+  const capitaliseString = () => string.toUpperCase();
+  const printString = () => console.log(`----${capitaliseString()}----`);
+  return { printString };
+};
+
+const taco = FactoryFunction("taco");
+
+// printString();
+// capitaliseString();
+// taco.capitaliseString();
+taco.printString();
+
+const counterCreator = () => {
+  let count = 0;
+  return () => {
+    console.log(count);
+    count++;
+  };
+};
+
+// console.log(count);
+
+const counter = counterCreator();
+
+counter();
+counter();
